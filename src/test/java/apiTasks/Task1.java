fc312092dbc9cac92bde8c97dbac4108d250d6a3
jasmina <jasminasevic@yahoo.com>
2022-01-24 00:53:42 +0100

    Create project for task 2 and add dependencies

---
+++ src/test/java/apiTasks/Task1.java
@@ -0,0 +1,14 @@
+package apiTasks;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Task1 {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
